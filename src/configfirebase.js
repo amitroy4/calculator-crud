ce3c9e02eae6f13c2c6ce4e987ddf95415195b8c
amitroy4 <amitroyrock6071@gmail.com>
2023-05-15 22:32:15 +0600

    upload basic folder

---
+++ src/configfirebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA1PeB3oj_wMTet9P08VUxUDUx-inxmvjU",
+    authDomain: "calculator-18f78.firebaseapp.com",
+    projectId: "calculator-18f78",
+    storageBucket: "calculator-18f78.appspot.com",
+    messagingSenderId: "845231998641",
+    appId: "1:845231998641:web:d041d6b8a62d8006b1c490"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig
