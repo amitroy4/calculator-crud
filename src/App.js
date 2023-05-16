0d9254464194a86ea77709bfb78db93a7a091414
amitroy4 <amitroyrock6071@gmail.com>
2023-05-16 23:36:16 +0600

    add history

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-import {getDatabase,ref,set,push} from "firebase/database";
-import { useState } from 'react';
+import { getDatabase, ref, set, push, onValue, remove, update } from "firebase/database";
+import { useEffect, useState } from 'react';
 
 function App() {
   const db = getDatabase();
-  let [text,setText] = useState("")
-  let handleSubmit = ()=>{
-    set(push(ref(db,'calculatorapp/')),{
-      value : text
-    }).then(()=>{
+  let [number, setNumber] = useState("")
+  let [numberArr, setNumberArr] = useState([])
+
+  let handleSubmit = () => {
+    set(push(ref(db, 'calculatorapp/')), {
+      prevalue: "0",
+      value: number,
+      operator: "Add",
+      total: 0 + number * 1,
+    }).then(() => {
       console.log("data send")
+    })
+  }
+
+  useEffect(() => {
+    const calRef = ref(db, 'calculatorapp/');
+    onValue(calRef, (snapshot) => {
+      let arr = []
+      snapshot.forEach((item) => {
+        arr.push({
+          ...item.val(),
+          id: item.key,
+        })
+      })
+      setNumberArr(arr)
+    })
+  })
+
+
+  let handleDelete = (id) => {
+    remove(ref(db, 'calculatorapp/' + id)).then(() => {
+      console.log("delete hoise")
     })
   }
 
@@ -23,7 +49,7 @@ function App() {
           <div className='flex justify-between'>
             <div>
               <h4>Addition</h4>
-              <input onChange={(e)=> setText(e.target.value)} />
+              <input onChange={(e) => setNumber(e.target.value)} />
             </div>
             <div>
               <h4>Substruction</h4>
@@ -32,7 +58,7 @@ function App() {
           </div>
 
           <div className='flex justify-center mt-[50px]'>
-            <button onClick ={handleSubmit} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded block">Button</button>
+            <button onClick={handleSubmit} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded block">Button</button>
           </div>
 
           <div className='text-center mt-[20px] mb-[50px]'>
@@ -54,6 +80,13 @@ function App() {
         </div>
         <div className='w-[600px] bg-red-400 p-5'>
           <div className='text-center font-medium text-2xl'>History</div>
+          <div>
+            {numberArr.map((item, index) => (
+              <li key={index}>We {item.operator} {item.prevalue} with {item.value} and result is {item.total}.
+                <button onClick={() => handleDelete(item.id)} class="bg-blue-300 hover:bg-blue-500 text-white text-xs font-normal py-1 px-1 border border-blue-500 rounded ml-[5px]">Delete</button>
+                <button class="bg-blue-300 hover:bg-blue-500 text-white text-xs font-normal py-1 px-1 border border-blue-500 rounded ml-[5px]">Edit</button></li>
+            ))}
+          </div>
         </div>
       </div>
     </section>
